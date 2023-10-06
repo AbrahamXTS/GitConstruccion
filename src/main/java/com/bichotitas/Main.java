85d3cabecb1a13b104262fd0da0f2426cbc952bd
Abraham Espinosa <abrahamespinosa3132@gmail.com>
2023-10-05 18:48:00 -0600

    fix: Now prints a different name

--- src/main/java/com/bichotitas/Main.java
+++ src/main/java/com/bichotitas/Main.java
@@ -2,6 +2,8 @@ package com.bichotitas;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Abraham!");
+        String name = "Adjany";
+
+        System.out.println("Hello " + name);
     }
 }
