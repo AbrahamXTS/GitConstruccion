c6424d1f7a3dc1c35239106b529a402ce3f309d9
Abraham Espinosa <abrahamespinosa3132@gmail.com>
2023-10-05 18:15:48 -0600

    feat: :sparkles: Change from Hello World to Hello Abraham
    Change the system.out.println from Hello World to Hello Abraham
    
    Closes #2

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+V
+&src/main/java/com/bichotitas/Main.java,f/9/f95dea28bf8608d82611e01594030744a5682e1d
--- src/main/java/com/bichotitas/Main.java
+++ src/main/java/com/bichotitas/Main.java
@@ -2,6 +2,6 @@ package com.bichotitas;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello Abraham!");
     }
 }
